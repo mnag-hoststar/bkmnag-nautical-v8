66f71b8176d68978daad626be6a09c47fa3830bf
Eva Jutzeler <eju@hoststar.ch>
2022-05-20 16:37:27 +0200

    #447 Nautical 2.0: Fix template bugs for migration

--- js/navigation.js
+++ js/navigation.js
@@ -2,23 +2,32 @@
 // Nautical: JS > Navigation
 // -------------------------------------
 
+$(document).ready(function () {
+	
+	// Toggles class open to display/hide the whole navigation
 
-// Toggles class open to display/hide the whole navigation
-$( ".widget__extendednavigation" ).addClass( "closed" );
+	$( ".widget__extendednavigation" ).addClass( "closed" );
 
-$( ".extendednavigation__navigation-toggle" ).click(function() {
-	$( ".widget__extendednavigation" ).toggleClass( "open" ).toggleClass( "closed" );
-});
-
-
-// Togglse class open to display/hide folders
-$( ".navigation-item.folder .item-name--parent" ).each(function() {
-	$( this ).click(function() {
-			$( this ).parent( ".navigation-item.folder" ).toggleClass( "open" );
+	$( ".extendednavigation__navigation-toggle" ).click(function() {
+		$( ".widget__extendednavigation" ).toggleClass( "open" ).toggleClass( "closed" );
 	});
-});
 
 
-$( ".extendednavigation__navigation-toggle" ).click(function() {
-    $( "html" ).toggleClass( "nav-open" );
+	// Togglse class open to display/hide folders
+
+	$( ".navigation-item.folder .item-name--parent" ).each(function() {
+		$( this ).click(function() {
+				$( this ).parent( ".navigation-item.folder" ).toggleClass( "open" );
+		});
+	});
+
+	$( ".extendednavigation__navigation-toggle" ).click(function() {
+		$( "html" ).toggleClass( "nav-open" );
+	});
+
+	// Hides other submenus when a main menu point with a submenu is clicked.
+    
+	$(".navigation-item").click(function() {
+		$(this).siblings(".navigation-item").removeClass("folder-open open");
+	});
 });
